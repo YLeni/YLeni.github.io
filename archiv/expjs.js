efcc296ca14eebea1c3c9c4b85c9e3f8c56c208f
datastorysurvey <43198040+YLeni@users.noreply.github.com>
2023-10-24 15:01:56 +1100

    update the new template

--- archiv/expjs.js
+++
@@ -1,8 +0,0 @@
-<script>
-$(document).on("keypress", "crowd-input", function(e){
-  if(e.which == 13){
-      var inputVal = $(this).val();
-      alert("You've entered: " + inputVal);
-  }
-});
-</script>
